944717a6159bb93dfc34bd5e8b56ae904db2d65e
Guillaume Charmetant <cguille.dev@gmail.com>
2013-11-06 23:44:16 +0100

    Provide the user (if any) in response.locals.user

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,5 @@
  * GET home page.
  */
 exports.index = function(request, response){
-    var locals = { title: 'Welcome' };
-
-    if (request.user) {
-        locals.user = request.user;
-    }
-    response.render('index', locals);
+    response.render('index', { title: 'Welcome' });
 };
